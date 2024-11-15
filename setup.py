2b78f5f19c4a6bb9ce164ea8e5e820d37d83905d
OZKON24 <za9474@yahoo.com>
2024-11-15 12:20:48 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+c = input(">>>: ")
+
+if c == "0":
+     os.system("pip install requests")
+     os.system("pip install progressbar")
+     os.system("pip install randomstring")
+     os.system("git pull")
+     
+elif c == "1":
+     os.system("pip3 install requests")
+     os.system("pip3 install progressbar")
+     os.system("pip3 install randomstring")
+     os.system("git pull")
+     print("Done.")
